20cd9b2f9a6bcafbc2d1b8af4a3e58d6eaab9fe5
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-03-24 17:10:04 -0400

    reads in file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
+    int collisions = 0;
+    // creating instance of hashmap
+    
     // loading the file
+    std::string line;
+    std::ifstream file ("/Users/kaseystowell/Documents/workspace/hashing/marvel-wikia-data.csv");
+    if (file.is_open()) {
+        while (getline(file,line)) {
+            std::cout << line << std::endl;
+        }
+        file.close();
+    }
+    else {
+        std::cout << "Unable to open file.";
+    }
     // creating instance of hashmap
     // creating objects
     // track number of collisions
